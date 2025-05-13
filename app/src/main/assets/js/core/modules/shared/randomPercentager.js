7133864c1fb448c5c9ca9e87a88e92a8111b706c
Marc Kooij <mrkooij@gmail.com>
2025-05-13 23:58:53 +0200

    Format randomPercentager indent 2 spaces

--- app/src/main/assets/js/core/modules/shared/randomPercentager.js
+++ app/src/main/assets/js/core/modules/shared/randomPercentager.js
@@ -2,13 +2,13 @@
 
 // 1. Namespace protection (ADD THIS FIRST)
 if (window.ChaosMachineUtils && window.ChaosMachineUtils.getRandomPercentage) {
-    throw new Error("ChaosMachineUtils conflict detected");
+  throw new Error("ChaosMachineUtils conflict detected");
 }
 
 // 2. Namespace initialization
 window.ChaosMachineUtils = window.ChaosMachineUtils || {};
 
 // 3. Function definition
-ChaosMachineUtils.getRandomPercentage = function(min, max) {
-    return `${min + Math.random() * (max - min)}%`;
+ChaosMachineUtils.getRandomPercentage = function (min, max) {
+  return `${min + Math.random() * (max - min)}%`;
 };
