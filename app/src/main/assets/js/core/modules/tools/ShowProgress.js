04d5a33822a2f7e05d1f5f57263ab8102c3def4a
Marc Kooij <mrkooij@gmail.com>
2025-05-14 21:43:21 +0200

    Clean up ShowProgress.js some more

--- app/src/main/assets/js/core/modules/tools/ShowProgress.js
+++ app/src/main/assets/js/core/modules/tools/ShowProgress.js
@@ -1,4 +1,5 @@
 // ShowProgress.js
+
 class ShowProgress {
   constructor() {
     this.name = 'show_progress';
@@ -11,7 +12,6 @@ class ShowProgress {
     this.abortController = new AbortController();
     const signal = this.abortController.signal;
     
-    // 9 seconds for progress loop
     const totalRuntimeMs = 9000;
     let accumulatedTimeMs = 0;
 
@@ -19,19 +19,15 @@ class ShowProgress {
       await machine.display("\n\tShow progress:\n\n");
       let percentage = 0;
 
-      // Continue until 100% or max runtime
       while (percentage < 100 && accumulatedTimeMs < totalRuntimeMs) {
         if (!machine.isRunning || this.shouldStop || signal.aborted) break;
 
-        // Random percentage increment
         const increment = 1 + Math.floor(Math.random() * 40);
         percentage = Math.min(99, percentage + increment);
 
-        // Calculate wait time proportional to percentage increment
         const waitTimeMs = (increment / 100) * totalRuntimeMs;
         accumulatedTimeMs += waitTimeMs;
 
-        // Wait before display of progress percentage
         try {
           await this.chunkedDelay(waitTimeMs, 200, signal);
         } catch (e) {
@@ -39,13 +35,11 @@ class ShowProgress {
           throw e;
         }
 
-        // Display progress percentage
         if (!machine.isRunning || this.shouldStop || signal.aborted) break;
 
         await machine.display(`\tProgress: ${Math.round(percentage)}%\n`);
       }
 
-      // Display 100% conclusion for 1 second
       if (machine.isRunning && !this.shouldStop && !signal.aborted) {
         await machine.display("\nProgress: 100%\n\n");
 
